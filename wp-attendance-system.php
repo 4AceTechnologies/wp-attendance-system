787f0ad7c4ba1825dcf856ef47cde157e9296316
Waqar <waqar@4acetech.com>
2013-02-16 02:49:20 +0500

    added plugin info

---
+++ wp-attendance-system.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: WP Attendance System
+Plugin URI: https://github.com/4AceTechnologies/wp-attendance-system
+Description: Attendance and Timesheet Tracking System that allows you to track the time in, out, and time spent by Users of WordPress.
+Version: 1.0
+Author: 4 Ace Technologies
+Author URI: http://www.4acetech.com
+License: GPL2
+*/
+
+
+
+?>
